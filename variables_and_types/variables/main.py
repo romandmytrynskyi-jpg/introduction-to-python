bc147aae6b5923bbd37717d7f0a46d5bd9ab0817
romandmytrynskyi-jpg <>
2025-10-23 15:04:27 +0000

    Variables

---
+++ variables_and_types/variables/main.py
@@ -0,0 +1,10 @@
+item_name = "Eggs" # Assign a string for the item name
+item_price = 3.99  # Assign a float for the price per unit
+item_stock_quantity = 120  # Assign an integer for the quantity in stock
+in_stock = True  # Assign a boolean value indicating if eggs are in stock
+
+# Testing
+print("Item : ", item_name)
+print("Price per unit: $", item_price)
+print("quantity in stock: ", item_stock_quantity)
+print("In stock: ", in_stock)
